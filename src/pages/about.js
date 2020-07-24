2cbe3e575d7285acd84b16d063f7d249169edb5f
Mugilan <mugilancodes@gmail.com>
2020-07-24 13:22:57 +0530

    Initial Commit

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,13 +1,11 @@
 import React from "react"
 import Header from "../components/header"
-import { Link } from "gatsby"
 
 export default function Home() {
   return (
-    <div style={{ color: `purple` }}>
-      <Link to="/contact/">Contact</Link>
-      <Header headerText="Hello Gatsby!" />
-      <p>What a world.</p>
+    <div style={{ color: `red` }}>
+      <Header headerText="About Gatsby" />
+      <Header headerText="Also Generated from Header Sub-Component" />
       <img src="https://source.unsplash.com/random/400x200" alt="" />
     </div>
   )
